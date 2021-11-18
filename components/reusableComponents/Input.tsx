f637201ffdf9f6c64b3189286847559ab7e6b025
Lisa Søndergaard <lisa3074@stud.kea.dk>
2021-11-18 10:48:32 +0100

    added reusableComponents folder

---
+++ components/reusableComponents/Input.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
+
+const Input = (props) => {
+  return (
+    <View>
+    
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Input;
